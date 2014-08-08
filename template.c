41785ba15ef3117ee1b08df373fe9f05117a80a0
tal shorer <tal.shorer@gmail.com>
2014-08-08 11:51:13 +0300

    template.c: a very small copy-paste skeleton with placeholders

---
+++ template.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/* FARNAME stands for "find and replace NAME" */
+static char DRIVER_NAME[] = "FARNAME";
+
+static int __init FARNAME_init(void)
+{
+	return 0;
+}
+module_init(FARNAME_init);
+
+static void __exit FARNAME_exit(void)
+{
+}
+module_exit(FARNAME_exit);
+
+
+MODULE_AUTHOR("Tal Shorer");
+MODULE_DESCRIPTION("FIXME");
+MODULE_VERSION("0.0");
+MODULE_LICENSE("GPL");
+
