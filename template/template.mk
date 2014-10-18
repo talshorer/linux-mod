8367840a776784cd5d0b3fd97c377f6f0ca8b5b6
Tal Shorer <tal.shorer@gmail.com>
2014-10-18 16:52:34 +0300

    changed makefiles to allow invoking them from any directory

--- template/template.mk
+++ template/template.mk
@@ -1,4 +1,6 @@
-include $(PWD)/../env.mk
+THIS_MAKEFILE_DIR=$(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+
+include $(THIS_MAKEFILE_DIR)/../env.mk
 
 all:
 	make -C $(KERNEL) M=$(PWD) modules
