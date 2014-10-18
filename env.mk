17f207fef36d2a7f2e14f044fc7211d4a1500849
Tal Shorer <tal.shorer@gmail.com>
2014-10-18 20:18:50 +0300

    move all makefiles to a better make system across the repository this change reduces code duplication in makefiles

--- env.mk
+++ env.mk
@@ -1,4 +1,15 @@
 KERNEL=/lib/modules/$(shell uname -r)/build
 
+default: all
+
 %.out: %.c
 	$(CC) $< -o $@
+
+kern_make:
+	make -C $(KERNEL) M=$(THIS_MAKEFILE_DIR) $(KERN_RULE)
+
+modules:
+	make kern_make KERN_RULE=modules
+
+modules-clean:
+	make kern_make KERN_RULE=clean
