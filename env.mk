3e981fda984963d667ba1e0922bb58e3d157d15e
Tal Shorer <tal.shorer@gmail.com>
2014-10-26 21:55:40 +0200

    env.mk: added rules for: 1. add cc flags for a specific binary 2. easily compile shared object libraries

--- env.mk
+++ env.mk
@@ -1,9 +1,13 @@
 KERNEL=/lib/modules/$(shell uname -r)/build
+LMOD_CFLAGS:=-Wall
 
 default: all
 
 %.out: %.c
-	$(CC) $< -o $@ -Wall
+	$(CC) $< -o $@ $(LMOD_CFLAGS) $($@_CFLAGS)
+
+lib%.so: %.c
+	$(CC) $< -o $@ -shared $(LMOD_CFLAGS)
 
 kern_make:
 	make -C $(KERNEL) M=$(THIS_MAKEFILE_DIR) $(KERN_RULE)
