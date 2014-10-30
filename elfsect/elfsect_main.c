576777b3331084cf038fea7eccbfc6c8936f832f
Tal Shorer <tal.shorer@gmail.com>
2014-10-30 22:08:35 +0200

    elfsect: added a debugfs file that lists return values of dummy functions that way we can test the local dummies file used for generating the dummies source against the debugfs file and see we have the same dummies in the same order

--- elfsect/elfsect_main.c
+++ elfsect/elfsect_main.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/debugfs.h>
 
 #include "elfsect.h"
 
@@ -10,24 +11,83 @@ extern elfsect_dummy __stop_dummies;
 
 static __elfsect_dummy_symbol struct { } __elfsect_dummies_empty;
 
-static int __init elfsect_init(void)
+static int elfsect_debugfs_dummies_show(struct seq_file *m, void *v)
 {
 	elfsect_dummy *fp;
-	pr_info("%s: initializing\n", MODULE_NAME);
 	for (fp = &__start_dummies; fp < &__stop_dummies; fp++)
-		pr_info("%s: %p %s\n", MODULE_NAME, fp, (*fp)());
+		seq_printf(m, "%s\n", (*fp)());
 	return 0;
+}
+
+static int elfsect_debugfs_dummies_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, elfsect_debugfs_dummies_show, inode->i_private);
+}
+
+#define elfsect_debugfs_dummies_fname "dummies"
+static const struct file_operations elfsect_debugfs_stat_fops = {
+	.open = elfsect_debugfs_dummies_open,
+	.release = single_release,
+	.read = seq_read,
+	.llseek = seq_lseek,
+};
+
+static struct dentry *elfsect_debugfs;
+
+static int __init elfsect_create_debugfs(void)
+{
+	int err;
+	struct dentry *file;
+
+	elfsect_debugfs = debugfs_create_dir(MODULE_NAME, NULL);
+	if (!elfsect_debugfs) {
+		err = -ENOMEM;
+		printk(KERN_ERR "%s: debugfs_create_dir failed\n", MODULE_NAME);
+		goto fail_debugfs_create_dir;
+	}
+
+	file = debugfs_create_file(elfsect_debugfs_dummies_fname, 0444,
+			elfsect_debugfs, NULL, &elfsect_debugfs_stat_fops);
+	if (!file) {
+		err = -ENOMEM;
+		printk(KERN_ERR "%s: debugfs_create_file for %s\n",
+				MODULE_NAME, elfsect_debugfs_dummies_fname);
+		goto fail_debugfs_create_file;
+	}
+
+	return 0;
+
+fail_debugfs_create_file:
+	debugfs_remove_recursive(elfsect_debugfs);
+fail_debugfs_create_dir:
+	return err;
+}
+
+static int __init elfsect_init(void)
+{
+	int err;
+
+	err = elfsect_create_debugfs();
+	if (err)
+		goto fail_elfsect_create_debugfs;
+
+	pr_info("%s: initialized successfully\n", MODULE_NAME);
+	return 0;
+
+fail_elfsect_create_debugfs:
+	return err;
 }
 module_init(elfsect_init);
 
 static void __exit elfsect_exit(void)
 {
-	pr_info("%s: exitting\n", MODULE_NAME);
+	debugfs_remove_recursive(elfsect_debugfs);
+	pr_info("%s: exited successfully\n", MODULE_NAME);
 }
 module_exit(elfsect_exit);
 
 
 MODULE_AUTHOR("Tal Shorer");
 MODULE_DESCRIPTION("A module with a section of dummy function pointers");
-MODULE_VERSION("1.1.2");
+MODULE_VERSION("1.2.0");
 MODULE_LICENSE("GPL");
--- elfsect/test.sh
+++ elfsect/test.sh
@@ -2,6 +2,9 @@
 
 MODULE=elfsect
 SECTION_NAME=.dummies
+DUMMIES_FILENAME=dummies
+LOCAL_DUMMIES="$DUMMIES_FILENAME.txt"
+DEBUGFS_DUMMIES="/sys/kernel/debug/$MODULE/$DUMMIES_FILENAME"
 
 err=0
 cd $(dirname $0)
@@ -13,5 +16,12 @@ else
 	err=1
 fi
 echo "$0: section $SECTION_NAME $exists in module $MODULE" 1>&2
+if cmp -s $LOCAL_DUMMIES $DEBUGFS_DUMMIES; then
+	match=""
+else
+	match="don't "
+	err=1
+fi
+echo "$0: $DUMMIES_FILENAME files in local directory and debufgs ${match}match"
 rmmod $MODULE
 exit $err
