0cfffaef94f3182953ba0a6b499b59cb1499f3b1
Tal Shorer <tal.shorer@gmail.com>
2014-12-27 11:30:15 +0200

    added support for building and uploading modules for a buildroot-made vm. extra environment variables such as CROSS_COMPILE and ARCH are required when compiling for the vm.
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ utils/send_vm.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+LOCAL_DIR=$1
+TARGET=$2
+TARGET_DIR=$TARGET/$(basename $LOCAL_DIR)
+
+function upload {
+	scp $1 $TARGET_DIR/$(basename $1)
+}
+
+function local_ls {
+	pattern=$1
+	ls $LOCAL_DIR/$pattern 2> /dev/null
+}
+
+ssh ${TARGET%:*} "mkdir -p ${TARGET_DIR#*:}"
+
+for pattern in "*.ko" "test.sh" "*.out"; do
+	for f in $(local_ls $pattern); do upload $f; done
+done
