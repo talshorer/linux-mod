a57fb738aaea5c05a917af330b0dfc7810c6132f
Tal Shorer <tal.shorer@gmail.com>
2015-01-13 21:48:01 +0200

    deepfs: don't use simple_fill_super
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- deepfs/deepfs_super.c
+++ deepfs/deepfs_super.c
@@ -5,6 +5,7 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/parser.h>
+#include <linux/pagemap.h>
 
 #define DEEPFS_MAGIC 0x0157ae10
 
@@ -58,17 +59,24 @@ static int deepfs_parse_options(char *data, struct deepfs_mount_opts *opts)
 	return 0;
 }
 
-static const struct super_operations deepfs_super_operations = {
+struct super_operations deepfs_super_ops = {
 	.statfs = simple_statfs,
+	.drop_inode = generic_delete_inode,
 };
 
 static int deepfs_fill_super(struct super_block *sb, void *data, int silent)
 {
-	static struct tree_descr files[] = {{""}};
 	struct deepfs_fs_info *fsi;
+	struct inode *root;
+	struct dentry *root_dentry;
 	int err;
 
 	save_mount_options(sb, data);
+
+	sb->s_blocksize = PAGE_CACHE_SIZE;
+	sb->s_blocksize_bits = PAGE_CACHE_SHIFT;
+	sb->s_magic = DEEPFS_MAGIC;
+	sb->s_op = &deepfs_super_ops;
 
 	fsi = kzalloc(sizeof(*fsi), GFP_KERNEL);
 	if (!fsi) {
@@ -82,15 +90,30 @@ static int deepfs_fill_super(struct super_block *sb, void *data, int silent)
 	if (err)
 		goto fail_deepfs_parse_options;
 
-	err = simple_fill_super(sb, DEEPFS_MAGIC, files);
-	if (err)
-		goto fail_simple_fill_super;
+	root = new_inode(sb);
+	if (!root) {
+		pr_err("failed to create root inode\n");
+		err = -ENOMEM;
+		goto fail_new_inode;
+	}
+	root->i_mode = S_IFDIR | 0755;
+	root->i_uid.val = 0;
+	root->i_gid.val = 0;
+	root->i_blocks = 0;
+	root->i_atime = root->i_mtime = root->i_ctime = CURRENT_TIME;
 
-	sb->s_op = &deepfs_super_operations;
+	root_dentry = d_make_root(root);
+	if (!root_dentry) {
+		pr_err("failed to create root dentry\n");
+		goto fail_d_make_root;
+	}
+	sb->s_root = root_dentry;
 
 	return 0;
 
-fail_simple_fill_super:
+fail_d_make_root:
+	iput(root);
+fail_new_inode:
 fail_deepfs_parse_options:
 	kfree(fsi);
 	sb->s_fs_info = NULL;
@@ -108,7 +131,7 @@ static struct dentry *deepfs_mount(struct file_system_type *fs_type, int flags,
 		const char *dev_name, void *data)
 {
 	pr_info("<%s>\n", __func__);
-	return mount_single(fs_type, flags, data, deepfs_fill_super);
+	return mount_nodev(fs_type, flags, data, deepfs_fill_super);
 }
 
 static struct file_system_type deepfs_fs_type = {
@@ -133,5 +156,5 @@ module_exit(deepfs_exit);
 
 MODULE_AUTHOR("Tal Shorer");
 MODULE_DESCRIPTION("Recursive pseudo file system");
-MODULE_VERSION("0.1.0");
+MODULE_VERSION("0.1.1");
 MODULE_LICENSE("GPL");
