cf8178cb6e7ea49ea3bec42d239fc50f7db45071
Tal Shorer <tal.shorer@gmail.com>
2015-01-24 20:44:29 +0200

    rtc-jiffies: module is functional. test is still not finished
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- rtc-jiffies/rtc-jiffies.c
+++ rtc-jiffies/rtc-jiffies.c
@@ -17,16 +17,58 @@ static int __init jrtc_check_module_params(void) {
 	return err;
 }
 
+struct jrtc_time {
+	unsigned long time;
+	unsigned long jiffies; /* typeof(jiffies) */
+};
+
+struct jrtc_clock {
+	struct device *dev;
+	struct rtc_device *rtc;
+	struct jrtc_time last_set;
+	spinlock_t lock; /* protects last_set */
+};
+
+#define to_jrtc_clock(dev) ((struct jrtc_clock *)dev_get_drvdata(dev))
+
 static int jrtc_read_time(struct device *dev, struct rtc_time *tm)
 {
-	/* TODO use rtc_time_to_tm */
-	return -ENODEV;
+	struct jrtc_clock *jc = to_jrtc_clock(dev);
+	struct jrtc_time jt;
+	unsigned long flags;
+
+	dev_info(dev, "<%s>\n", __func__);
+
+	spin_lock_irqsave(&jc->lock, flags);
+	memcpy(&jt, &jc->last_set, sizeof(jt));
+	spin_unlock_irqrestore(&jc->lock, flags);
+
+	rtc_time_to_tm(jt.time + (jiffies - jt.jiffies) / HZ, tm);
+
+	return 0;
 }
 
 static int jrtc_set_time(struct device *dev, struct rtc_time *tm)
 {
-	/* TODO use rtc_tm_time */
-	return -ENODEV;
+	struct jrtc_clock *jc = to_jrtc_clock(dev);
+	struct jrtc_time jt;
+	unsigned long flags;
+	int err;
+
+	dev_info(dev, "<%s>\n", __func__);
+
+	err = rtc_tm_to_time(tm, &jt.time);
+	if (err) {
+		dev_err(dev, "rtc_tm_to_time failed, err = %d\n", err);
+		return err;
+	}
+	jt.jiffies = jiffies;
+
+	spin_lock_irqsave(&jc->lock, flags);
+	memcpy(&jc->last_set, &jt, sizeof(jt));
+	spin_unlock_irqrestore(&jc->lock, flags);
+
+	return 0;
 }
 
 static const struct rtc_class_ops jrtc_rtc_ops = {
@@ -34,26 +76,49 @@ static const struct rtc_class_ops jrtc_rtc_ops = {
 	.set_time	= jrtc_set_time,
 };
 
-struct jrtc_clock {
-	struct device *dev;
-	struct rtc_device *rtc;
-	unsigned long last_set_time;
-	unsigned long last_set_jiffies; /* typeof(jiffies) */
-};
-
 static struct jrtc_clock *jrtc_clocks;
 
 static struct class *jrtc_class;
+#define JRTC_MKDEV(i) MKDEV(0, i)
 
 static int jrtc_clock_setup(struct jrtc_clock *jc, int i)
 {
-	/* TODO */
+	int err;
+
+	jc->last_set.jiffies = jiffies;
+	spin_lock_init(&jc->lock);
+
+	jc->dev = device_create(jrtc_class, NULL, JRTC_MKDEV(i), jc,
+			"%s%d", jrtc_class->name, i);
+	if (IS_ERR(jc->dev)) {
+		err = PTR_ERR(jc->dev);
+		pr_err("device_create failed. i = %d, err = %d\n", i, err);
+		goto fail_device_create;
+	}
+
+	jc->rtc = rtc_device_register(dev_name(jc->dev), jc->dev,
+			&jrtc_rtc_ops, THIS_MODULE);
+	if (IS_ERR(jc->rtc)) {
+		err = PTR_ERR(jc->rtc);
+		pr_err("rtc_device_register failed. i = %d, err = %d\n",
+				i, err);
+		goto fail_rtc_device_register;
+	}
+
+	dev_info(jc->dev, "created successfully\n");
 	return 0;
+
+fail_rtc_device_register:
+	device_destroy(jrtc_class, jc->dev->devt);
+fail_device_create:
+	return err;
 }
 
 static void jrtc_clock_cleanup(struct jrtc_clock *jc)
 {
-	/* TODO */
+	dev_info(jc->dev, "destroying\n");
+	rtc_device_unregister(jc->rtc);
+	device_destroy(jrtc_class, jc->dev->devt);
 }
 
 static int __init jrtc_init(void)
@@ -117,5 +182,5 @@ module_exit(jrtc_exit);
 
 MODULE_AUTHOR("Tal Shorer");
 MODULE_DESCRIPTION("Virtual real time clocks that work on system ticks");
-MODULE_VERSION("0.1.0");
+MODULE_VERSION("1.0.0");
 MODULE_LICENSE("GPL");
--- rtc-jiffies/test.sh
+++ rtc-jiffies/test.sh
@@ -2,6 +2,8 @@
 
 MODULE=$(basename $(dirname $(realpath $0)))
 NCLOCKS=4
+KBUILD_MODNAME=$(echo $MODULE | tr '-' '_')
+MODULE_SYSFS=/sys/class/$KBUILD_MODNAME
 # used to protect access to output streams
 LOCKFILE=$(mktemp)
 
@@ -10,26 +12,46 @@ __echo()
 	flock $LOCKFILE -c echo "$@" 1>&2
 }
 
+check_diff()
+{
+	local rtcdev=$1
+	local sysclk=$(date +%c)
+	local hwclk=$(hwclock -f $rtcdev -r |
+			sed "s/\(.*\)  0\.0\{6\}\ seconds/\1/")
+	[[ "$sysclk" == "$hwclk" ]] && return 0
+	# TODO allow a difference of up to one second
+}
+
 test_one_clock()
 {
 	local clock=$1
 	local lerr=0
-	__echo "beginning test on clock $clock"
-	# test logic
-	__echo "finished test on clock $clock, err=$lerr"
+	local clock_sysfs=$MODULE_SYSFS/$clock
+	local rtc=$(basename $clock_sysfs/rtc*)
+	local rtcdev=/dev/$rtc
+	__echo "$0: beginning test on clock $clock ($rtc)"
+	hwclock -f $rtcdev -w
+	# TODO for each value in (0, 2, 4) sleep $value and run check_diff
+	__echo "$0: unfinished TODO"
+	lerr=1
+	__echo "$0: finished test on clock $clock, err=$lerr"
 	[[ $lerr != 0 ]] && err=$lerr
 }
 
 err=0
 cd $(dirname $0)
 insmod $MODULE.ko nclocks=$NCLOCKS
-__echo "testing $NCLOCKS clocks in parallel"
+__echo "$0: testing $NCLOCKS clocks in parallel"
 children=""
 for i in $(seq 0 $(( $NCLOCKS - 1 ))); do
-	test_one_clock $i &
+	test_one_clock ${KBUILD_MODNAME}$i &
 	children="$children $!"
 done
 wait $children
+# debug start
+echo "hit return to finish test"
+read
+# debug end
 rmmod $MODULE
 rm $LOCKFILE
 exit $err
