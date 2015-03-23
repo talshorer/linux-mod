d1c9554c5dfc55c46f2ed52e676469796b6664a2
Tal Shorer <tal.shorer@gmail.com>
2015-03-23 20:16:33 +0200

    simplat: add a dummy devres with size 0
    This is done purely to poke at the devres mechanism by asserting
    that the release function is called during module removal.
    
    It is not tested by simplat/test.sh because there appears to not
    be an indication about the devres in sysfs.
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- simplat/simplat.c
+++ simplat/simplat.c
@@ -9,9 +9,24 @@
 /* not needed */
 /* #define DRIVER_NAME "simplat" */
 
+static void simplat_dummy_release(struct device *dev, void *res)
+{
+	dev_info(dev, "<%s>\n", __func__);
+}
+
 static int simplat_probe(struct platform_device *pdev)
 {
+	void *res;
+
 	dev_info(&pdev->dev, "<%s>\n", __func__);
+
+	res = devres_alloc(simplat_dummy_release, 0, GFP_KERNEL);
+	if (!res) {
+		dev_err(&pdev->dev, "devres_alloc failed\n");
+		return -ENOMEM;
+	}
+	devres_add(&pdev->dev, res);
+
 	return 0;
 }
 
@@ -34,4 +49,4 @@ module_platform_driver(simplat_driver);
 
 LMOD_MODULE_META();
 MODULE_DESCRIPTION("A simple platform driver that does nothing");
-MODULE_VERSION("1.0.0");
+MODULE_VERSION("1.1.0");
