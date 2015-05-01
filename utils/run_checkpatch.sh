a4ae6228ae5f29151d22879502e9f914f8f86a2b
Tal Shorer <tal.shorer@gmail.com>
2015-05-01 15:05:28 +0300

    utils: run_checkpatch: use the -f option when running checkpatch.pl
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- utils/run_checkpatch.sh
+++ utils/run_checkpatch.sh
@@ -3,28 +3,18 @@
 UTILS=$(realpath $(dirname $0))
 CHECKPATCH=$KERNEL/scripts/checkpatch.pl
 TMPOUTPUT=$(mktemp)
-TMPPATCH=$(mktemp)
 files="$@"
 errors=0
-
-function __run_checkpatch {
-	local file=$1
-	rm -f $TMPPATCH
-	echo "Signed-off-by: Dummy" >> $TMPPATCH
-	diff -urN /dev/null $file >> $TMPPATCH
-	$CHECKPATCH $TMPPATCH
-	return $?
-}
 
 if [[ -z "$files" ]]; then files=$($UTILS/all_files.sh); fi
 
 for file in $files; do
 	echo $file
-	__run_checkpatch $file &> $TMPOUTPUT
+	$CHECKPATCH -f $file &> $TMPOUTPUT
 	if [[ $? != 0 ]]; then
 		let errors+=1
 		cat $TMPOUTPUT
 	fi
 done
 echo "errors: $errors"
-rm -rf $TMPOUTPUT $TMPPATCH
+rm -rf $TMPOUTPUT
