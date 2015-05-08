78f76cf74f91a76ac4c27db6662d3be84e1cdc2e
Tal Shorer <tal.shorer@gmail.com>
2015-05-08 14:06:29 +0300

    utils: all_files: prints pathes relative to linux-mod
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- utils/all_files.sh
+++ utils/all_files.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 LMOD=$(realpath $(dirname $0)/..)
-find $LMOD -path "$LMOD/.git*" -prune -o -type f -print
+find $LMOD -path "$LMOD/.git*" -prune -o -type f -print | sed "s#$LMOD/##"
