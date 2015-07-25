588dbc72bbce95543f6e135b5e044d9a249c90b9
Tal Shorer <tal.shorer@gmail.com>
2015-07-25 11:39:50 +0300

    simpleattr: use pr_fmt and remove DRIVER_NAME
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- simpleattr/simpleattr.c
+++ simpleattr/simpleattr.c
@@ -1,3 +1,5 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
@@ -8,8 +10,6 @@
 #include <lmod/meta.h>
 
 #define SIMPLEATTR_SYSFS_PERM (0644)
-
-static const char DRIVER_NAME[] = "simpleattr";
 
 static int simpleattr_ndevices = 1;
 module_param_named(ndevices, simpleattr_ndevices, int, 0);
@@ -31,11 +31,11 @@ static void simpleattr_print_sys_attr_access(struct device *dev,
 		struct device_attribute *attr, unsigned long val,
 		const char *func)
 {
-	pr_info("%s: sysfs access: %s", DRIVER_NAME, func);
-	pr_info("%s: \tdevice %s\n", DRIVER_NAME, dev->kobj.name);
-	pr_info("%s: \tattribute %s\n", DRIVER_NAME, attr->attr.name);
-	pr_info("%s: \tprocess %d\n", DRIVER_NAME, current->pid);
-	pr_info("%s: \tvalue %lu\n", DRIVER_NAME, val);
+	pr_info("sysfs access: %s", func);
+	pr_info("\tdevice %s\n", dev->kobj.name);
+	pr_info("\tattribute %s\n", attr->attr.name);
+	pr_info("\tprocess %d\n", current->pid);
+	pr_info("\tvalue %lu\n", val);
 }
 
 static ssize_t simpleattr_sys_attr_show(struct device *dev,
@@ -69,23 +69,18 @@ static struct device __init *simpleattr_device_create(int i)
 	int err;
 
 	dev = device_create(simpleattr_class, NULL, simpleattr_MKDEV(i), NULL,
-			"%s%d", DRIVER_NAME, i);
+			"%s%d", KBUILD_MODNAME, i);
 	if (IS_ERR(dev)) {
 		err = PTR_ERR(dev);
-		pr_err(
-		"%s: device_create failed. i = %d, err = %d\n",
-				DRIVER_NAME, i, err);
+		pr_err("device_create failed. i = %d, err = %d\n", i, err);
 		goto fail_device_create;
 	}
 	err = device_create_file(dev, &dev_attr_attr);
 	if (err) {
-		pr_err(
-		"%s: device_create_file failed. i = %d, err = %d\n",
-				DRIVER_NAME, i, err);
+		pr_err("device_create_file failed. i = %d, err = %d\n",i, err);
 		goto fail_device_create_file;
 	}
-	pr_info("%s: created device %s successfully\n",
-			DRIVER_NAME, dev->kobj.name);
+	pr_info("created device %s successfully\n", dev->kobj.name);
 	return dev;
 fail_device_create_file:
 	device_destroy(simpleattr_class, simpleattr_MKDEV(i));
@@ -97,8 +92,7 @@ static void simpleattr_device_destroy(int i)
 {
 	struct device *dev = simpleattr_devices[i];
 
-	pr_info("%s: destroying device %s\n",
-			DRIVER_NAME, dev->kobj.name);
+	pr_info("destroying device %s\n", dev->kobj.name);
 	device_remove_file(dev, &dev_attr_attr);
 	device_destroy(simpleattr_class, simpleattr_MKDEV(i));
 }
@@ -109,38 +103,36 @@ static int __init simpleattr_init(void)
 	int i;
 	struct device *dev;
 
-	pr_info("%s: in %s\n", DRIVER_NAME, __func__);
+	pr_info("in %s\n", __func__);
 	if (simpleattr_ndevices < 0) {
-		pr_err("%s: simpleattr_ndevices < 0. value = %d\n",
-				DRIVER_NAME, simpleattr_ndevices);
+		pr_err("simpleattr_ndevices < 0. value = %d\n",
+				simpleattr_ndevices);
 		return -EINVAL;
 	}
 	simpleattr_devices = kmalloc(
 			sizeof(simpleattr_devices[0]) * simpleattr_ndevices,
 			GFP_KERNEL);
 	if (!simpleattr_devices) {
-		pr_err("%s: failed to allocate simpleattr_devices\n",
-				DRIVER_NAME);
+		pr_err("failed to allocate simpleattr_devices\n");
 		err = -ENOMEM;
 		goto fail_kmalloc_simpleattr_devices;
 	}
-	simpleattr_class = class_create(THIS_MODULE, DRIVER_NAME);
+	simpleattr_class = class_create(THIS_MODULE, KBUILD_MODNAME);
 	if (IS_ERR(simpleattr_class)) {
 		err = PTR_ERR(simpleattr_class);
-		pr_err("%s: class_create failed. err = %d\n",
-				DRIVER_NAME, err);
+		pr_err("class_create failed. err = %d\n", err);
 		goto fail_class_create;
 	}
 	for (i = 0; i < simpleattr_ndevices; i++) {
 		dev = simpleattr_device_create(i);
 		if (IS_ERR(dev)) {
 			err = PTR_ERR(dev);
-			pr_err("%s: unwinding\n", DRIVER_NAME);
+			pr_err("unwinding\n");
 			goto fail_simpleattr_device_create_loop;
 		}
 		simpleattr_devices[i] = dev;
 	}
-	pr_info("%s: initialized successfully\n", DRIVER_NAME);
+	pr_info("initialized successfully\n");
 	return 0;
 fail_simpleattr_device_create_loop:
 	/* device at [i] isn't created */
@@ -158,16 +150,16 @@ static void __exit simpleattr_exit(void)
 {
 	int i;
 
-	pr_info("%s: in %s\n", DRIVER_NAME, __func__);
+	pr_info("in %s\n", __func__);
 	for (i = 0; i < simpleattr_ndevices; i++)
 		simpleattr_device_destroy(i);
 	class_destroy(simpleattr_class);
 	kfree(simpleattr_devices);
-	pr_info("%s: exited successfully\n", DRIVER_NAME);
+	pr_info("exited successfully\n");
 }
 module_exit(simpleattr_exit);
 
 LMOD_MODULE_AUTHOR();
 LMOD_MODULE_LICENSE();
 MODULE_DESCRIPTION("A simple dummy device with a sysfs attribute");
-MODULE_VERSION("1.2.2");
+MODULE_VERSION("1.2.3");
