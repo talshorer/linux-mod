708057c0de46256f4d38d5a0210aa0a2a40375aa
Tal Shorer <tal.shorer@gmail.com>
2016-07-22 16:41:29 +0300

    echoserial: change uapi number (kernel took the one used)
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- echoserial/echoserial_uapi.h
+++ echoserial/echoserial_uapi.h
@@ -2,6 +2,6 @@
 #define _ECHOSERIAL_UAPI_H
 
 /* one more than the last type @ include/uapi/linux/serial_core.h */
-#define PORT_ECHOSERIAL 114
+#define PORT_ECHOSERIAL 117
 
 #endif /* _ECHOSERIAL_UAPI_H */
