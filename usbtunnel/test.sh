0f95e7986a78eaea80a1d0c3ffd4ac4910f6dae3
Tal Shorer <tal.shorer@gmail.com>
2016-12-18 16:41:37 +0200

    usbtunnel: tunnel list management (via sysfs)
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- usbtunnel/test.sh
+++ usbtunnel/test.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
 MODULE=$(basename $(dirname $(realpath $0)))
+HOST_DRIVER_SYSFS="/sys/bus/usb/drivers/${MODULE}_host"
+HOST_DRIVER_MATCH="$HOST_DRIVER_SYSFS/match"
 
 err=0
 cd $(dirname $0)
 insmod $MODULE.ko
 modprobe dummy_hcd num=2
+port=$(ls -1 /sys/devices/platform/dummy_hcd.0/usb*/ | grep "^[0-9]\+-[0-9]\+$")
+udc="dummy_udc.1"
+echo "add $port $udc" > $HOST_DRIVER_MATCH
 # test logic
 err=1
 rmmod dummy_hcd
--- usbtunnel/usbtunnel.c
+++ usbtunnel/usbtunnel.c
@@ -5,11 +5,33 @@
 #include <linux/usb.h>
 #include <linux/usb/gadget.h>
 
+#define MAX_DEVNAME_LEN 32
+
 struct usbtunnel {
-	char *udc_name;
+	char udc[MAX_DEVNAME_LEN + 1];
+	char port[MAX_DEVNAME_LEN + 1];
 	struct usb_gadget_driver gadget_driver;
 	struct usb_device *udev;
+	struct list_head list;
 };
+
+static LIST_HEAD(usbtunnel_list);
+static DEFINE_SPINLOCK(usbtunnel_list_lock);
+
+static struct usbtunnel *usbtunnel_lookup(const char *port)
+{
+	struct usbtunnel *ret;
+	unsigned long flags;
+
+	spin_lock_irqsave(&usbtunnel_list_lock, flags);
+	list_for_each_entry(ret, &usbtunnel_list, list)
+		if (!strcmp(port, ret->port))
+			goto found;
+	ret = NULL;
+found:
+	spin_unlock_irqrestore(&usbtunnel_list_lock, flags);
+	return ret;
+}
 
 static int usbtunnel_host_probe(struct usb_device *udev)
 {
@@ -24,15 +46,159 @@ static void usbtunnel_host_disconnect(struct usb_device *udev)
 
 static ssize_t match_show(struct device_driver *driver, char *buf)
 {
+	size_t ret = 0;
+	unsigned long flags;
+	struct usbtunnel *ut;
+
+	spin_lock_irqsave(&usbtunnel_list_lock, flags);
+	list_for_each_entry(ut, &usbtunnel_list, list)
+		ret += snprintf(&buf[ret], PAGE_SIZE - ret, "%s %s\n",
+			ut->port, ut->udc);
+	spin_unlock_irqrestore(&usbtunnel_list_lock, flags);
+
+	return ret;
+}
+
+static int usbtunnel_gadget_bind(struct usb_gadget *gadget,
+		struct usb_gadget_driver *gdriver)
+{
 	/* TODO */
+	return -ENODEV;
+}
+
+static void usbtunnel_gadget_unbind(struct usb_gadget *gadget)
+{
+	/* TODO */
+}
+
+static int usbtunnel_gadget_setup(struct usb_gadget *gadget,
+		const struct usb_ctrlrequest *ctrl)
+{
+	/* TODO */
+	return -EINVAL;
+}
+
+static void usbtunnel_gadget_disconnect(struct usb_gadget *gadget)
+{
+	/* TODO */
+}
+
+static void usbtunnel_gadget_suspend(struct usb_gadget *gadget)
+{
+	/* TODO */
+}
+
+static void usbtunnel_gadget_resume(struct usb_gadget *gadget)
+{
+	/* TODO */
+}
+
+static const struct usb_gadget_driver usbtunnel_gadget_driver_template = {
+	.bind       = usbtunnel_gadget_bind,
+	.unbind     = usbtunnel_gadget_unbind,
+
+	.setup      = usbtunnel_gadget_setup,
+	.reset      = usbtunnel_gadget_disconnect,
+	.disconnect = usbtunnel_gadget_disconnect,
+
+	.suspend    = usbtunnel_gadget_suspend,
+	.resume     = usbtunnel_gadget_resume,
+
+	/* TODO function? */
+	/* TODO max_speed? */
+
+	.driver	= {
+		/* TODO name? */
+		.owner = THIS_MODULE,
+	},
+};
+
+static int usbtunnel_add(const char *buf, size_t len)
+{
+	const char *sep;
+	size_t portlen;
+	size_t udclen;
+	struct usbtunnel *ut;
+	unsigned long flags;
+
+	if (buf[len - 1] == '\n')
+		len--;
+	sep = memchr(buf, ' ', len);
+	if (!sep)
+		return -EINVAL;
+	portlen = sep - buf;
+	udclen = len - 1 - portlen;
+	if (portlen > MAX_DEVNAME_LEN || udclen > MAX_DEVNAME_LEN)
+		return -EINVAL;
+	sep++;
+	ut = kzalloc(sizeof(*ut), GFP_KERNEL);
+	if (!ut)
+		return -ENOMEM;
+
+	memcpy(ut->port, buf, portlen);
+	memcpy(ut->udc, sep, udclen);
+	pr_info("tunneling port %s to udc %s\n", ut->port, ut->udc);
+	ut->gadget_driver = usbtunnel_gadget_driver_template;
+	/* TODO more fields. udc? */
+	/* TODO probe */
+
+	spin_lock_irqsave(&usbtunnel_list_lock, flags);
+	list_add(&ut->list, &usbtunnel_list);
+	spin_unlock_irqrestore(&usbtunnel_list_lock, flags);
+
+	return 0;
+}
+
+static void usbtunnel_cleanup(struct usbtunnel *ut)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&usbtunnel_list_lock, flags);
+	list_del(&ut->list);
+	spin_unlock_irqrestore(&usbtunnel_list_lock, flags);
+
+	pr_info("removed tunnel on port %s\n", ut->port);
+	kfree(ut);
+}
+
+static int usbtunnel_del(const char *buf, size_t len)
+{
+	char *s;
+	struct usbtunnel *ut;
+
+	if (buf[len - 1] == '\n')
+		len--;
+	s = kmemdup(buf, len + 1, GFP_KERNEL);
+	if (!s)
+		return -ENOMEM;
+	s[len] = '\0';
+	ut = usbtunnel_lookup(s);
+	kfree(s);
+	if (!ut)
+		return -EINVAL;
+	usbtunnel_cleanup(ut);
 	return 0;
 }
 
 static ssize_t match_store(struct device_driver *driver, const char *buf,
 		size_t count)
 {
-	/* TODO */
-	return count;
+	/*
+	 * one of:
+	 * ... "add PORT UDC\n"
+	 * ... "del PORT\n"
+	 * the '\n' is optional.
+	 */
+	int err;
+
+	if (!strncmp(buf, "add ", 4))
+		err = usbtunnel_add(buf + 4, count - 4) ?: count;
+	else if (!strncmp(buf, "del ", 4))
+		err = usbtunnel_del(buf + 4, count - 4) ?: count;
+	else
+		err = -EINVAL;
+
+	return err ?: count;
 }
 
 static DRIVER_ATTR_RW(match);
@@ -67,7 +233,18 @@ module_init(usbtunnel_init);
 
 static void __exit usbtunnel_exit(void)
 {
+	struct usbtunnel *ut;
+	struct usbtunnel *tmp;
+	unsigned long flags;
+
 	usb_deregister_device_driver(&usbtunnel_host_driver);
+	spin_lock_irqsave(&usbtunnel_list_lock, flags);
+	list_for_each_entry_safe(ut, tmp, &usbtunnel_list, list) {
+		spin_unlock_irqrestore(&usbtunnel_list_lock, flags);
+		usbtunnel_cleanup(ut);
+		spin_lock_irqsave(&usbtunnel_list_lock, flags);
+	}
+	spin_unlock_irqrestore(&usbtunnel_list_lock, flags);
 }
 module_exit(usbtunnel_exit);
 
